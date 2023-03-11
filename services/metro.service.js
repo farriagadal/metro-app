f36c32d23806e3fddaae88be9995e664f25a416a
Fernando <farriagadal94@gmail.com>
2023-03-11 18:06:22 -0300

    add metro service

--- services/metro.service.js
+++ services/metro.service.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 // import api from './api'
 
-export const getList = () => {
+export const getMetroLines = () => {
   return new Promise((resolve) => {
-    axios.post('/cliente')
+    const data = new FormData()
+    data.append('resource_id', '3d54e961-d81b-4507-aeee-7a433e00a9bf')
+    axios.post('https://datos.gob.cl/api/3/action/datastore_search', data)
       .then((response) => {
         resolve(response.data)
       })
